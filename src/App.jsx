cb509280134b35af0c3a95fff17f26beffe4a1c7
Aydin Terzi <aydinterzi7@gmail.com>
2024-04-13 22:44:16 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,137 @@
+import { useState } from "react";
+import MoonIcon from "./assets/icon-moon.svg";
+import CheckIcon from "./assets/icon-check.svg";
+
 function App() {
-  return <div className="h-dvh">test</div>;
+  const [todos, setTodos] = useState([
+    { id: 1, todo: "Complete online JavaScript course", isCompleted: false },
+    { id: 2, todo: "Jog around the park 3x", isCompleted: false },
+    { id: 3, todo: "10 minutes meditation", isCompleted: true },
+    { id: 4, todo: "Read for 1 hour", isCompleted: false },
+    { id: 5, todo: "Pick up groceries", isCompleted: false },
+    { id: 6, todo: "Complete Todo App on Frontend Mentor", isCompleted: false },
+  ]);
+
+  const [filteredTodos, setFilteredTodos] = useState([...todos]);
+
+  const handleClick = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleAdd = (e) => {
+    if (e.key === "Enter") {
+      const newTodos = {
+        id: todos.length + 1,
+        todo: e.target.value,
+        isCompleted: false,
+      };
+      setTodos((prev) => [...prev, newTodos]);
+      setFilteredTodos((prev) => [...prev, newTodos]);
+      e.target.value = "";
+    }
+  };
+
+  return (
+    <div className="h-dvh bg-hero-pattern-light bg-auto bg-repeat-x bg-very-light-gray flex justify-center text-lg">
+      <div className="w-2/5 mt-20 flex flex-col space-y-10">
+        <div className="flex items-center justify-between">
+          <span className="text-very-light-gray font-semibold text-5xl tracking-widest">
+            TODO
+          </span>
+          <img src={MoonIcon} alt="moon icon" />
+        </div>
+        <div className="p-4 flex items-center space-x-5 bg-very-light-gray rounded-md font-semibold text-light-grayish-blue w-full">
+          <div className="border rounded-full w-5 h-5"></div>
+          <input
+            type="text"
+            className="w-full p-2 text-very-dark-grayish-blue"
+            placeholder="Create a new todo..."
+            onKeyDown={handleAdd}
+          />
+        </div>
+        <div className="w-full flex flex-col  border rounded-md bg-white shadow-lg">
+          {filteredTodos.map((todo) => (
+            <div
+              key={todo.id}
+              className="p-4 flex items-center space-x-5 border-t"
+              onClick={() => handleClick(todo.id)}
+            >
+              <div
+                className={`flex items-center justify-center rounded-full w-6 h-6 ${
+                  todo.isCompleted
+                    ? "bg-gradient-to-r from-violet-500 to-fuchsia-500"
+                    : "border"
+                }`}
+              >
+                {todo.isCompleted && (
+                  <img src={CheckIcon} alt="check icon" className="h-3 w-3" />
+                )}
+              </div>
+              <span
+                className={`text-dark-grayish-blue ${
+                  todo.isCompleted && "line-through"
+                }`}
+              >
+                {todo.todo}
+              </span>
+            </div>
+          ))}
+          <div className="flex justify-between px-5 py-3 border-t text-base">
+            <span className="text-dark-grayish-blue">
+              {todos.filter((todo) => !todo.isCompleted).length} items left
+            </span>
+            <div className="flex space-x-3 font-semibold">
+              <span
+                className="cursor-pointer"
+                onClick={() => setFilteredTodos([...todos])}
+              >
+                All
+              </span>
+              <span
+                onClick={() =>
+                  setFilteredTodos([
+                    ...todos.filter((todo) => !todo.isCompleted),
+                  ])
+                }
+                className="cursor-pointer"
+              >
+                Active
+              </span>
+              <span
+                onClick={() =>
+                  setFilteredTodos([
+                    ...todos.filter((todo) => todo.isCompleted),
+                  ])
+                }
+                className="cursor-pointer"
+              >
+                Completed
+              </span>
+            </div>
+            <span
+              onClick={() => {
+                setTodos((prev) => [
+                  ...prev.filter((todo) => !todo.isCompleted),
+                ]);
+                setFilteredTodos((prev) => [
+                  ...prev.filter((todo) => !todo.isCompleted),
+                ]);
+              }}
+              className="text-dark-grayish-blue cursor-pointer"
+            >
+              Clear Completed
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern-dark": "url('bg-desktop-dark.jpg')",
+        "hero-pattern-light": "url('bg-desktop-light.jpg')",
+      },
+      colors: {
+        "bright-blue": "hsl(220, 98%, 61%)",
+        "check-background":
+          "linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+        "very-light-grayish-blue": "hsl(236, 33%, 92%)",
+        "light-grayish-blue": "hsl(233, 11%, 84%)",
+        "dark-grayish-blue": "hsl(236, 9%, 61%)",
+        "very-dark-grayish-blue": "hsl(235, 19%, 35%)",
+        "very-dark-blue": "hsl(235, 21%, 11%)",
+        "very-dark-desaturated-blue": "hsl(235, 24%, 19%)",
+        "light-grayish-blue-hover": "hsl(236, 33%, 92%)",
+      },
+    },
   },
   plugins: [],
 };
